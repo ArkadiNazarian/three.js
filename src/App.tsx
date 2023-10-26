0dfe546644a2306fe6fea7e6e98d314c9c471640
ArkadiNazarian <nazarianarkadi@gmail.com>
2023-10-26 18:34:14 +0330

    asddd

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { Three } from './three';
+
 
 function App() {
+
+  const [v, set_v] = useState<string>()
+  const handlechange = async (e: any) => {
+    const file = await e.target.files[0];
+    const blob = URL.createObjectURL(file);
+    let a = document.createElement("a");
+    document.body.appendChild(a);
+    a.style.display = "none";
+    a.href = blob;
+    a.download = "fileName.stl";
+    // a.click()
+    // console.log(blob)
+    // set_v(blob)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100vw', height: '100vh' }}>
+      {/* {
+        v && <Three stl={v}/>
+      } */}
+      <Three stl="https://s3.amazonaws.com/minifactory-stl/WALLY_1plate.stl" />
+      <input type='file' onChange={(e) => handlechange(e)} title='upload' />
     </div>
   );
 }
 
 export default App;
+
+// https://d.staging.layers.app/stl/6536819982df3d65319a3f3f/stl/a7ed796f-e9c8-4b4c-ab12-617bb6fdddb8.stl
---
+++ src/three.tsx
@@ -0,0 +1,141 @@
+import { useEffect, useRef, useState } from 'react';
+import './App.css';
+import * as THREE from 'three';
+import { Canvas, useLoader } from '@react-three/fiber';
+// Deai - R3F
+import { OrbitControls } from '@react-three/drei';
+import { STLLoader } from 'three/examples/jsm/loaders/STLLoader';
+import { STLExporter } from 'three/examples/jsm/exporters/STLExporter';
+const Model = (props: { selectedStl: string; ratio: number; set_ratio: any; }) => {
+    const meshRef = useRef(null);
+
+    let material = new THREE.MeshLambertMaterial({ color: "ffffff" });
+    // "https://s3.amazonaws.com/minifactory-stl/WALLY_1plate.stl"
+
+    let geometry = useLoader(STLLoader, '/e.stl').center();
+
+    useEffect(() => {
+        const xSize = geometry.boundingBox!.max.x;
+        const ySize = geometry.boundingBox!.max.y;
+        const zSize = geometry.boundingBox!.max.z;
+        let maxSizeHelper = Math.max(xSize, ySize, zSize);
+        geometry.scale(2 / maxSizeHelper, 2 / maxSizeHelper, 2 / maxSizeHelper);
+        props.set_ratio((2 * props.ratio) / maxSizeHelper);
+
+    }, [props])
+
+    // useEffect(()=>{
+    //     const exporter = new STLExporter();
+    //     const options = { binary: true }
+    //     const b = new THREE.Mesh(geometry, material)
+    //     const result = exporter.parse(b, options);
+    //     console.log(result)
+    // },[geometry])
+
+
+    return (
+        <group rotation={[-Math.PI / 2, 0, -Math.PI / 2]} position={[0, geometry.boundingBox!.max.z, 0]}>
+            <mesh
+                // scale
+                castShadow
+                // receiveShadow
+                ref={meshRef}
+                // raycast
+                geometry={geometry}
+                material={material}
+            />
+            {/* {cubes} */}
+            {/* {triangles} */}
+        </group>
+    )
+}
+
+export const Three = (props: { stl: string }) => {
+    const canvasRef = useRef(null);
+    const [ratio, set_ratio] = useState(1)
+
+
+    const meshRef = useRef(null);
+
+    let material = new THREE.MeshLambertMaterial({ color: "ffffff" });
+    // "https://s3.amazonaws.com/minifactory-stl/WALLY_1plate.stl"
+
+    let geometry = useLoader(STLLoader, props.stl).center();
+
+    useEffect(() => {
+        const xSize = geometry.boundingBox!.max.x;
+        const ySize = geometry.boundingBox!.max.y;
+        const zSize = geometry.boundingBox!.max.z;
+        let maxSizeHelper = Math.max(xSize, ySize, zSize);
+        geometry.scale(2 / maxSizeHelper, 2 / maxSizeHelper, 2 / maxSizeHelper);
+        set_ratio((2 * ratio) / maxSizeHelper);
+
+    }, [props])
+
+    return (
+        <div
+            ref={canvasRef}
+            style={{ width: '40.926vw', height: '44.508vw' }}
+        >
+            {/* Our Scene & Camera is already built into our canvas */}
+            <Canvas camera={{ position: [-5, 2, 10], fov: 60 }}>
+                {props.stl && (
+                    <gridHelper
+                        args={[
+                            100000 * 1 * ratio,
+                            10000,
+                            0x444444,
+                            0x050505,
+                        ]}
+                    />
+                )}
+                <axesHelper args={[50]} />
+                {/* This light makes things look pretty */}
+                <ambientLight intensity={0.3} />
+                {/* Our main source of light, also casting our shadow */}
+                <directionalLight
+                    castShadow
+                    position={[10, 10, 10]}
+                    intensity={1.5}
+                    shadow-mapSize-width={1024}
+                    shadow-mapSize-height={1024}
+                    shadow-camera-far={50}
+                    shadow-camera-left={-10}
+                    shadow-camera-right={10}
+                    shadow-camera-top={10}
+                    shadow-camera-bottom={-10}
+                />
+                {/* A light to help illumnate the spinning boxes */}
+                <pointLight position={[-10, 0, -20]} intensity={0.5} />
+                <pointLight position={[0, -10, 0]} intensity={1.5} />
+                <group>
+                    {/* This mesh is the plane (The floor) */}
+                    <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]} receiveShadow>
+                        {/* <PlaneGeometry attach="geometry" args={[100, 100]} /> */}
+                        <shadowMaterial attach="material" opacity={0.3} />
+                    </mesh>
+                    {/* <SpinningMesh
+                position={[0, 1, 0]}
+                color='lightblue'
+                args={[3, 2, 1]}
+                speed={2}
+              /> */}
+                    {props.stl && (
+
+                        <Model
+                            ratio={ratio}
+                            set_ratio={set_ratio}
+                            // stlColor={props.stlColor}
+                            // maxSize={maxSize}
+                            // stlBlob={stlBlob}
+                            selectedStl={props.stl}
+                        />
+
+                    )}
+                </group>
+                {/* Allows us to move the canvas around for different prespectives */}
+                <OrbitControls />
+            </Canvas>
+        </div>
+    );
+}
